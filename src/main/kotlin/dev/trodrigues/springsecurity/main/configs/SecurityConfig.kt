3c7d865a95b8aeb12a59196bdddf9e26de2cebd1
Thiago Rodrigues <hey@thiagorodrigues.dev>
2022-08-13 17:13:07 -0300

    feat: add Spring Security

---
+++ src/main/kotlin/dev/trodrigues/springsecurity/main/configs/SecurityConfig.kt
@@ -0,0 +1,20 @@
+package dev.trodrigues.springsecurity.main.configs
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .authorizeHttpRequests { auth ->
+                auth.anyRequest().permitAll()
+            }
+        return http.build()
+    }
+}
